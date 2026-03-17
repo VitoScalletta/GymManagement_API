425b981d6c6a5440a6d861ab0641c48f837947e6
VitoScalletta <muhammetemre3737@gmail.com>
2026-03-17 14:30:36 +0300

    ModelMapper konfigürasyonu yapıldı

--- src/main/java/org/example/gymmanagement_api/config/MapperConfig.java
+++ src/main/java/org/example/gymmanagement_api/config/MapperConfig.java
@@ -1,4 +1,18 @@
 package org.example.gymmanagement_api.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MapperConfig {
+    @Bean
+    public ModelMapper getModelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
+    }
 }
