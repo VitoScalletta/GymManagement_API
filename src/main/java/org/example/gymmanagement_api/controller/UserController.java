732ef35de6475a3869a28f49140a3f94d646a4d7
VitoScalletta <muhammetemre3737@gmail.com>
2026-03-26 15:08:15 +0300

    UserController CRUD işlemleri tamamlandı

--- src/main/java/org/example/gymmanagement_api/controller/UserController.java
+++ src/main/java/org/example/gymmanagement_api/controller/UserController.java
@@ -6,10 +6,9 @@ import org.example.gymmanagement_api.dto.request.UserRequestDto;
 import org.example.gymmanagement_api.dto.response.UserResponseDto;
 import org.example.gymmanagement_api.service.interfaces.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,6 +18,27 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserRequestDto userRequestDto) {
+        return ResponseEntity.ok(userService.createUser(userRequestDto));
+    }
 
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id, @Valid @RequestBody UserRequestDto userRequestDto) {
+        return ResponseEntity.ok(userService.updateUser(id, userRequestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserResponseDto> deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
     }
 }
