9b12fd965fe68506b08a5700ba1f7a46183809ec
VitoScalletta <muhammetemre3737@gmail.com>
2026-04-02 19:30:36 +0300

    Post metotları yazıldı

--- src/main/java/org/example/gymmanagement_api/controller/WorkoutProgramController.java
+++ src/main/java/org/example/gymmanagement_api/controller/WorkoutProgramController.java
@@ -1,9 +1,34 @@
 package org.example.gymmanagement_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.gymmanagement_api.dto.request.ExerciseRequestDto;
+import org.example.gymmanagement_api.dto.request.WorkoutProgramRequestDto;
+import org.example.gymmanagement_api.dto.response.ExerciseResponseDto;
+import org.example.gymmanagement_api.dto.response.WorkoutProgramResponseDto;
+import org.example.gymmanagement_api.service.interfaces.WorkoutProgramService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/workout_program")
+@RequiredArgsConstructor
 public class WorkoutProgramController {
+    private final WorkoutProgramService workoutProgramService;
+
+    @PostMapping("/member/{memberId}")
+    public ResponseEntity<WorkoutProgramResponseDto> createProgram(
+            @PathVariable Long memberId,
+            @Valid @RequestBody WorkoutProgramRequestDto requestDto
+    ){
+        return ResponseEntity.ok(workoutProgramService.createWorkoutProgram(memberId, requestDto));
+    }
+
+    @PostMapping("/{programId}/exercises")
+    public ResponseEntity<ExerciseResponseDto> addExerciseToProgram(
+            @PathVariable Long programId,
+            @Valid @RequestBody ExerciseRequestDto requestDto
+    ){
+        return ResponseEntity.ok(workoutProgramService.addExerciseToProgram(programId, requestDto));
+    }
 }
