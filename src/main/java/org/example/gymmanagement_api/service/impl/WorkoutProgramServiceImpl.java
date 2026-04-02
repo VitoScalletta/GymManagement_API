2ed41477d817f90e08e4d6d0c64b5de2eaf2684b
VitoScalletta <muhammetemre3737@gmail.com>
2026-04-02 13:52:54 +0300

    WorkoutProgramService Metotları oluşturuldu bu createWorkoutProgram metodu dolduruldu

--- src/main/java/org/example/gymmanagement_api/service/impl/WorkoutProgramServiceImpl.java
+++ src/main/java/org/example/gymmanagement_api/service/impl/WorkoutProgramServiceImpl.java
@@ -1,4 +1,49 @@
 package org.example.gymmanagement_api.service.impl;
 
-public class WorkoutProgramServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.gymmanagement_api.dto.request.ExerciseRequestDto;
+import org.example.gymmanagement_api.dto.request.WorkoutProgramRequestDto;
+import org.example.gymmanagement_api.dto.response.ExerciseResponseDto;
+import org.example.gymmanagement_api.dto.response.WorkoutProgramResponseDto;
+import org.example.gymmanagement_api.entity.Role;
+import org.example.gymmanagement_api.entity.User;
+import org.example.gymmanagement_api.entity.WorkoutProgram;
+import org.example.gymmanagement_api.exception.BusinessRuleException;
+import org.example.gymmanagement_api.exception.ResourceNotFoundException;
+import org.example.gymmanagement_api.repository.ExerciseRepository;
+import org.example.gymmanagement_api.repository.UserRepository;
+import org.example.gymmanagement_api.repository.WorkoutProgramRepository;
+import org.example.gymmanagement_api.service.interfaces.UserService;
+import org.example.gymmanagement_api.service.interfaces.WorkoutProgramService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WorkoutProgramServiceImpl implements WorkoutProgramService {
+    private final WorkoutProgramRepository workoutProgramRepository;
+    private final ExerciseRepository exerciseRepository;
+    private final ModelMapper modelMapper;
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+    @Override
+    public WorkoutProgramResponseDto createWorkoutProgram(Long memberId, WorkoutProgramRequestDto requestDto) {
+        User currentUser = userService.getCurrenUser();
+        if (currentUser.getRole() != Role.TRAINER) {
+            throw new BusinessRuleException("Antreman Programı oluşturamazsın");
+        }
+
+        User member = userRepository.findById(memberId)
+                .orElseThrow(() -> new ResourceNotFoundException("Aradığınız üye bulunamadı"));
+        WorkoutProgram workoutProgram = modelMapper.map(requestDto, WorkoutProgram.class);
+        workoutProgram.setUser(member);
+        WorkoutProgram savedProgram = workoutProgramRepository.save(workoutProgram);
+        return modelMapper.map(workoutProgram , WorkoutProgramResponseDto.class);
+    }
+
+    @Override
+    public ExerciseResponseDto addExerciseToProgram(Long prorgramId, ExerciseRequestDto requestDto) {
+        return null;
+    }
 }
--- src/main/java/org/example/gymmanagement_api/service/interfaces/WorkoutProgramService.java
+++ src/main/java/org/example/gymmanagement_api/service/interfaces/WorkoutProgramService.java
@@ -1,6 +1,11 @@
 package org.example.gymmanagement_api.service.interfaces;
 
+import org.example.gymmanagement_api.dto.request.ExerciseRequestDto;
+import org.example.gymmanagement_api.dto.request.WorkoutProgramRequestDto;
+import org.example.gymmanagement_api.dto.response.ExerciseResponseDto;
+import org.example.gymmanagement_api.dto.response.WorkoutProgramResponseDto;
+
 public interface WorkoutProgramService {
-
-
+    WorkoutProgramResponseDto createWorkoutProgram(Long memberId,WorkoutProgramRequestDto requestDto);
+    ExerciseResponseDto addExerciseToProgram(Long prorgramId, ExerciseRequestDto requestDto);
 }
